7d6383bd008a9a5e6332dbfa6b2a3ecfa556a892
Jailton Domingos <development.j@Outlook.com>
2021-12-30 14:14:51 -0300

    [ReactJS] Pupop component stile (FEATURE)

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import './Popup.css'
+import GenericButton from './GenericButton'
 
 function Popup( props ) {
+    //setError, buttonPopup, setButtonPopup, message, error
+    //Clean error
+    const PopupClose = () => {
+        props.setButtonPopup(false)
+    }
 
-    return ( props.trigger) ? (
+    return ( props.buttonPopup ) ? (
         <div className='popup'>
             <div className="popup-inner">
-                <button className="close-btn" onClick={() => props.setTrigger(false)} >Button</button>
-                { props.children }
+                <h3> {props.message} </h3>
+                <GenericButton className='close-btn' event={ PopupClose } message={'Fechar'}/>
+                
             </div>
         </div>
     ) : ""
 }
 
 export default Popup
+//{ props.children }
+//<button className="close-btn" onClick={() => props.setTrigger(false)} >Button</button>

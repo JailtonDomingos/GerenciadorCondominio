c8f0b21d632c5c4c44c0ab77e87336acbf399bf3
Jailton Domingos <development.j@Outlook.com>
2021-12-30 14:16:16 -0300

    [ReactJS] Login page finished (FIX)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState} from "react";
 import LoginForm from "./components/LoginForm";
+import GenericButton from "./components/GenericButton";
 
 function App() {
   const adminUser = { 
     email: 'admin@admin.com',
-    password: 'admin' 
+    password: 'admin'
   }
 
   const [user, setUser] = useState({name: "", email: ""});
@@ -13,12 +14,15 @@ function App() {
 
   const Login = details => {
     console.log(details)
+
      if (details.email === adminUser.email && details.password === adminUser.password) {
       console.log('Logged in')
       setUser({
         name: details.name, 
         email: details.email
+        
       })
+      setError('')
      } else {
         setError('Invalid password!')
     }
@@ -33,13 +37,14 @@ function App() {
 
   return (
     <div className="App"> 
-      {(user.email != '') ? ( //Verificar futuramente com uma variable true que virá da validação do hash de login
-        <div className="Welcome"> 
+      {(user.email !== '') ? ( //Verificar futuramente com uma variable true que virá da validação do hash de login
+        
+        <div className="welcome"> 
           <h2>Welcome, <span>{user.name}</span></h2>
-          <button onClick={Logout}>Logout</button>
+          <GenericButton event={Logout} message={'Logout'}/>
         </div>
       ) : (
-        <LoginForm Login={Login} error={error} trigger={buttonPopup} setTrigger={setButtonPopup}/>
+        <LoginForm Login={Login} error={error} setError={setError} buttonPopup={buttonPopup} setButtonPopup={setButtonPopup}/>
       )} 
     </div>
 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react'
 import Popup from "./Popup";
 
-function LoginForm({ Login, error, trigger, setTrigger }) {
+function LoginForm( props ) {
+    //Login, error, buttonPopup, setButtonPopup
     const [details, setDetails] = useState({name: '', email: '', password: ''})
 
     const submitHandler = e => {
         e.preventDefault();
-        Login(details)    
+        props.Login(details)    
     }
 
     return (
         <form onSubmit={submitHandler}> 
             <div className="form-inner">
                 <h2>Login</h2>
-                {(error != '') ? ( <Popup trigger={trigger} setTrigger={setTrigger}><h3>Invalid Password!</h3></Popup> ) : ''}
+                {(props.error !== '') ? ( <Popup buttonPopup={props.buttonPopup} setButtonPopup={props.setButtonPopup} message={'Invalid User/Password'}></Popup> ) : ''}
                 <div className="form-group">
                     <label htmlFor="Name">Name</label>
                     <input type="name" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})}/>
@@ -26,13 +27,15 @@ function LoginForm({ Login, error, trigger, setTrigger }) {
                     <label htmlFor="password">Password</label>
                     <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})}/>
                 </div>
-                <input className="button-submit" type="submit" value="LOGIN" onClick={() => setTrigger(true)}/>
+                <input className="button-submit" type="submit" value="LOGIN" onClick={() => props.setButtonPopup(true)}/>
             </div>
         </form>
     )
 }
 
 export default LoginForm
+
+// Futuramente criar um component para Button do tipo Input 'Button-submit'
 
 // Notas que eu estou passando meus valores de detail pela variavel que já esta vindo no parametro 'Login' pela constante details criada na classe
 // Investigar se é possível fazer isso para a classe popup com as variaveis de setTriger

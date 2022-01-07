12f060b9e71c20f1e71af85b86708a083001c789
Jailton Domingos <development.j@Outlook.com>
2022-01-07 20:15:50 -0300

    [ReactJS] Apartamentos.js class Component

---
+++ src/components/Apartamentos.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Apartamentos() {
+    return (
+        <div 
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItens: 'center',
+                height: '90vh'
+            }}
+        >
+            <h3>Apartamentos</h3>
+        </div>
+    )
+}
+
+export default Apartamentos
